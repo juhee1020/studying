5d4186f3894dafb943f75991da2de3551f6e0634
juhee12368 <akg33329@gmail.com>
2022-12-31 14:17:35 +0900

    codingtest_practice

---
+++ CodeingTest/study/src/code_study/SolutionMain.java
@@ -0,0 +1,9 @@
+package code_study;
+
+public class SolutionMain {
+
+	public static void main(String[] args) {
+		Solution2 sss=new Solution2();
+		System.out.println("결과 : "+sss.solution("123456", "789789"));
+	}
+}
