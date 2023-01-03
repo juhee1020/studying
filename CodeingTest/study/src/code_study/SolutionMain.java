c5c4dcf2dc4e69f111b3f3ccdd7dceeb5f9939e7
juhee12368 <akg33329@gmail.com>
2023-01-03 12:48:28 +0900

    codingtest

--- CodeingTest/study/src/code_study/SolutionMain.java
+++ CodeingTest/study/src/code_study/SolutionMain.java
@@ -6,7 +6,10 @@ public class SolutionMain {
 		//Solution2 sss=new Solution2();
 		//System.out.println("결과 : "+sss.solution("123456", "789789"));
 
-		Solution3 ssss=new Solution3();		
-		System.out.println(ssss.solution("1239874", "512"));
+		//Solution3 ssss=new Solution3();		
+		//System.out.println(ssss.solution("1239874", "512"));
+		
+		//Solution4 sss=new Solution4();
+		//System.out.println(sss.solution("abcdefg1234"));
 	}
 }
