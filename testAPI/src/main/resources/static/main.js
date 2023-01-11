943ebea7c1b009a446851d6a919a8e46d8e46810
juhee12368 <akg33329@gmail.com>
2023-01-11 18:07:11 +0900

    지도 셀랙트 되는거에 따라 마커 찍기 o  → 센터 위치도 셀랙에 따라 설정되도록 해야된다.

--- testAPI/src/main/resources/static/main.js
+++ testAPI/src/main/resources/static/main.js
@@ -1,14 +1,25 @@
-async function getDate(){
+async function getDate(area_select){
     const url='http://apis.data.go.kr/1543061/animalShelterSrvc/shelterInfo?serviceKey=1u9OcPZBNH0dLUlpKbDQ9ZTFjhByrP49Hmk5dWIYwJl8QRbOWm4kj7NsH9%2FxJzsCtUnLpdYzVW%2BpnTIu2JznFQ%3D%3D&numOfRows=186&pageNo=1&_type=json';
     // serviceKey : 인증키 / numOfRows : 총 센터가 186개라서 / pageNo : 한페이지에 186개 다 뽑겠다. / _type : json형식으로 받겠다.
     const response=await fetch(url);
     const data=await response.json();
-    console.log();
-    const locations=data.response.body.items.item.filter(spot => spot.orgNm.substr(0,2)=='서울');
+
+    const locations=data.response.body.items.item.filter(spot => spot.orgNm.substr(0,2)==area_select);
+
     drawMap(locations);
 }
 getDate();
 
+
+function selectArea(){
+    var area_select=document.getElementById("area");
+    area_select=area_select.options[area_select.selectedIndex].value;
+    getDate(area_select);
+}
+
+
+// 카카오 - 여러개 마커에 이벤트 등록하기1
+// https://apis.map.kakao.com/web/sample/multipleMarkerEvent/
 
 function drawMap(locations){
     var mapContainer = document.getElementById('map'), // 지도를 표시할 div
@@ -43,17 +54,20 @@ function drawMap(locations){
         var marker = new kakao.maps.Marker({
             map: map, // 마커를 표시할 지도
             position: positions[i].latlng, // 마커를 표시할 위치
-            //title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-            //image : markerImage // 마커 이미지 - 없어도 됨.
         });
         // 마커에 표시할 인포윈도우를 생성합니다
         var infowindow = new kakao.maps.InfoWindow({
             content: positions[i].title // 인포윈도우에 표시할 내용
         });
+
+        // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
+        // 이벤트 리스너로는 클로저를 만들어 등록합니다
+        // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
         kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
         kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
     }
 
+    // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
     function makeOverListener(map, marker, infowindow) {
         return function() {
             infowindow.open(map, marker);
